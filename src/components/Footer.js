55f68060f2244315d0275039eaa1f67ea1a03654
Kadir Korkmaz <kadir33korkmaz@gmail.com>
2022-03-22 12:06:27 +0300

    add filter button selected styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import { useState } from 'react';
 
 
 function Footer({setStatus}) {
+    const [isSelected, setIsSelected] = useState ("all");
 
     const handleStatus = (e)=> {
+        
         if (e.target.className.includes("active")) {
             setStatus("active")
+            setIsSelected("active")
+
 
         }else if(e.target.className.includes("completed")){
             setStatus("completed")
+            setIsSelected("completed")
+
 
         }
          else if(e.target.className.includes("all")) {
             setStatus("all")
+            setIsSelected("all")
+
         }
 
 
@@ -29,13 +38,13 @@ function Footer({setStatus}) {
 
 <ul className="filters">
     <li>
-        <button onClick={handleStatus} className="all selected">All</button>
+        <button onClick={handleStatus} className= {`all ${isSelected === "all" ? "selected" :"" }`}>All</button>
     </li>
     <li>
-        <button onClick={handleStatus} className="active ">Active</button>
+        <button onClick={handleStatus} className={`active ${isSelected === "active" ? "selected" :"" }`}>Active</button>
     </li>
     <li>
-        <button onClick={handleStatus} className="completed ">Completed</button>
+        <button onClick={handleStatus} className={`completed ${isSelected === "completed" ? "selected" :"" }`}>Completed</button>
     </li>
 </ul>
 
